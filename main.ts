38352e0dd0ce3094e748dea24a8f2fe55036adad
kecy <keishiro.oyama@kecy.me>
2024-05-15 23:19:12 +0900

    feat: Implement basic features

--- main.ts
+++ main.ts
@@ -1,85 +1,74 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, TFile, ItemView, WorkspaceLeaf, MarkdownRenderer, Component } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ChecklistPluginSettings {
+	autoUpdate: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: ChecklistPluginSettings = {
+	autoUpdate: true
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const CHECKLIST_VIEW_TYPE = 'checklist-view';
+
+export default class ChecklistPlugin extends Plugin {
+	settings: ChecklistPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.registerView(
+			CHECKLIST_VIEW_TYPE,
+			(leaf) => new ChecklistView(leaf, this)
+		);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.addSettingTab(new ChecklistSettingTab(this.app, this));
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		if (this.settings.autoUpdate) {
+			this.registerEvent(
+				this.app.vault.on('modify', () => {
+					this.updateActiveChecklistView();
+				})
+			);
+			this.registerEvent(
+				this.app.vault.on('create', () => {
+					this.updateActiveChecklistView();
+				})
+			);
+			this.registerEvent(
+				this.app.vault.on('delete', () => {
+					this.updateActiveChecklistView();
+				})
+			);
+		}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.activateView();
 	}
 
 	onunload() {
+		this.app.workspace.detachLeavesOfType(CHECKLIST_VIEW_TYPE);
+	}
 
+	async activateView() {
+		this.app.workspace.detachLeavesOfType(CHECKLIST_VIEW_TYPE);
+
+		let rightLeaf = this.app.workspace.getRightLeaf(false);
+		if (!rightLeaf) {
+			rightLeaf = this.app.workspace.createLeafBySplit(this.app.workspace.getLeavesOfType('empty')[0], 'horizontal');
+		}
+
+		await rightLeaf.setViewState({
+			type: CHECKLIST_VIEW_TYPE,
+			active: true,
+		});
+
+		this.app.workspace.revealLeaf(this.app.workspace.getLeavesOfType(CHECKLIST_VIEW_TYPE)[0]);
+	}
+
+	async updateActiveChecklistView() {
+		const view = this.app.workspace.getLeavesOfType(CHECKLIST_VIEW_TYPE).first()?.view as ChecklistView;
+		if (view) {
+			view.updateChecklist();
+		}
 	}
 
 	async loadSettings() {
@@ -91,43 +80,105 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class ChecklistView extends ItemView {
+	private checklistContainer: HTMLElement;
+	private tomatoCountContainer: HTMLElement;
+	private plugin: ChecklistPlugin;
+
+	constructor(leaf: WorkspaceLeaf, plugin: ChecklistPlugin) {
+		super(leaf);
+		this.plugin = plugin;
+		this.tomatoCountContainer = this.contentEl.createDiv({ cls: 'tomato-count-container' });
+		this.checklistContainer = this.contentEl.createDiv({ cls: 'checklist-container' });
+		this.updateChecklist = this.updateChecklist.bind(this);
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	getViewType() {
+		return CHECKLIST_VIEW_TYPE;
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	getDisplayText() {
+		return "Pomotasks";
+	}
+
+	getIcon(): string {
+		return 'book-check';
+	}
+
+	async onOpen() {
+		this.updateChecklist();
+	}
+
+	async updateChecklist() {
+		const files = this.plugin.app.vault.getMarkdownFiles();
+		let checklistItems: { task: string, path: string, line: string }[] = [];
+		let tomatoCount = 0;
+
+		for (const file of files) {
+			const content = await this.plugin.app.vault.read(file);
+			const lines = content.split('\n');
+
+			for (const line of lines) {
+				if (line.match(/^\s*-\s*\[\s\]/)) {
+					checklistItems.push({ task: line.trim(), path: file.path, line });
+					tomatoCount += (line.match(/🍅/g) || []).length;
+				}
+			}
+		}
+
+		this.tomatoCountContainer.setText(`Total Tomatoes: ${tomatoCount}`);
+		this.checklistContainer.empty();
+
+		const checklistContent = this.checklistContainer.createDiv({ cls: 'checklist-list' });
+		
+		checklistItems.forEach(async (item) => {
+			const itemEl = checklistContent.createEl('label', { cls: 'checklist-item' });
+
+			const checkbox = itemEl.createEl('input');
+			checkbox.type = 'checkbox';
+			checkbox.onclick = async () => {
+				await this.markItemAsDone(item);
+			};
+
+			const markdownContainer = itemEl.createDiv();
+			await MarkdownRenderer.render(this.plugin.app, item.task.slice(6), markdownContainer, item.path, this);
+		});
+	}
+
+	async markItemAsDone(item: { task: string, path: string, line: string }) {
+		const file = this.plugin.app.vault.getAbstractFileByPath(item.path) as TFile;
+
+		if (file) {
+			const content = await this.plugin.app.vault.read(file);
+			const updatedContent = content.replace(item.line, item.line.replace('[ ]', '[x]'));
+			await this.plugin.app.vault.modify(file, updatedContent);
+			this.updateChecklist();
+		}
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class ChecklistSettingTab extends PluginSettingTab {
+	plugin: ChecklistPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ChecklistPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		containerEl.createEl('h2', { text: 'Checklist Plugin Settings' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Auto Update')
+			.setDesc('Automatically update checklist when files are modified.')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.autoUpdate)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.autoUpdate = value;
 					await this.plugin.saveSettings();
 				}));
 	}
